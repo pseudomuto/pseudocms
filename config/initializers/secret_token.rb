5f114d8d0a70c437e0ec465b908dfb90996dfbe4
pseudomuto <david.muto@gmail.com>
2014-02-02 23:08:20 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pseudocms::Application.config.secret_key_base = 'd413d095fd94eb4b08bc17d6dbd18687d7b7da2cce877e84f167775d32b3aaa9899a6682329fda4f6d9ab3a2c7c330fd931b901bf1001315ce403ca85fea779c'
